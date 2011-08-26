026e2acf0ae48aa9689bb6f4a627d908195478a7
phpfluesterer@googlemail.com <phpfluesterer@googlemail.com@ef6f09ba-08d5-11df-a843-f99e4352f152>
2011-08-26 07:20:59 +0000

    added the changes as recommended by romain: https://code.google.com/p/moovrelocator/issues/detail?id=3

--- trunk/lib/Moovrelocator.class.php
+++ trunk/lib/Moovrelocator.class.php
@@ -63,12 +63,12 @@
  * @package    Moovrelocator_Lib
  * @subpackage Moovrelocator_Lib_Base
  * @author     Benjamin Carl <phpfluesterer@googlemail.com>
- * @copyright  2009 Benjamin Carl
+ * @copyright  2009 - 2011 Benjamin Carl
  * @license    http://www.opensource.org/licenses/bsd-license.php The BSD License
  * @version    SVN: $Id$
  * @link       http://www.benjamin-carl.de
  * @see        -
- * @since      File available since Release 1.0.0
+ * @since      File available since Release 0.1
  */
 
 // get absolute path to this lib
@@ -93,12 +93,12 @@ require_once PATH_MOOVRELOCATOR.'bytes'.DIRECTORY_SEPARATOR.'Transform.class.php
  * @package    Moovrelocator_Lib
  * @subpackage Moovrelocator_Lib_Base
  * @author     Benjamin Carl <phpfluesterer@googlemail.com>
- * @copyright  2009 Benjamin Carl
+ * @copyright  2009 - 2011 Benjamin Carl
  * @license    http://www.opensource.org/licenses/bsd-license.php The BSD License
  * @version    Release: @package_version@
  * @link       http://www.benjamin-carl.de
  * @see        -
- * @since      File available since Release 1.0.0
+ * @since      File available since Release 0.1
  */
 class Moovrelocator
 {
@@ -197,7 +197,7 @@ class Moovrelocator
     /**
      * sets the input file to process
      *
-     * this method sets the input file 
+     * this method sets the input file
      *
      * @param string $filename The input file to fix
      *
@@ -209,24 +209,27 @@ class Moovrelocator
      */
     public function setInput($filename = null)
     {
+    	// reset previously retrieved results
+    	$this->_reset();
+
         // check and open file
         if (!file_exists($filename) || !($this->_fp = fopen($filename, 'rb'))) {
             return 'cannot open file: '.$filename;
         }
-        
+
         // get size of file
         $this->_filesize = filesize($filename);
-        
+
         // empty file?
         if ($this->_filesize == 0) {
-            return 'file '.$filename.' seems to be empyt!';
+            return 'file '.$filename.' seems to be empty!';
         }
-        
+
         // parse file's atoms
         while (!feof($this->_fp) && ftell($this->_fp) < $this->_filesize) {
             $this->_fileAtoms[] = $this->_parseAtomsFromInput();
         }
-        
+
         $moovEOFCheck = $this->_moovAtomAtEOF();
         if ($moovEOFCheck !== true) {
             return $moovEOFCheck;
@@ -234,7 +237,7 @@ class Moovrelocator
             // successful parsed the file!
             $this->_successfullyParsed = true;
         }
-        
+
         // success
         return true;
     }
@@ -261,28 +264,28 @@ class Moovrelocator
                 fseek($this->_fp, $position);
             }
         }
-        
+
         // get 8 bytes -> get totalSize and boxType
         $atomData = unpack('NtotalSize/NboxType', fread($this->_fp, 8));
         // store offset, size and type
         $offset = ftell($this->_fp) - 8;
         $size = $atomData['totalSize'];
         $type = pack('N', $atomData['boxType']);
-        
+
         // check 64 bit size (files > 4GB)
         if ($size == 1) {
             $highInt = $atomData['totalSize'];
             $size = ($highInt << 32) | $atomData['totalSize'];
         }
-        
+
         // positioning of file-pointer
         fseek($this->_fp, ($size + $offset));
-        
+
         // give back atom
         return self::factory($offset, $size, $type);
     }
 
-    
+
     /**
      * checks for valid qt-file
      *
@@ -300,24 +303,24 @@ class Moovrelocator
         if (!$this->_successfullyParsed) {
             return 'please open a file first!';
         }
-        
+
         // get first atom
         $firstAtom = $this->_fileAtoms[0];
-        
+
         // check first for being valid (first atom MUST be ftyp)
         if ($firstAtom->getType() != Atom::FTYP_ATOM) {
             $this->_fileValid = false;
             return 'encountered non-QT top-level atom (is this a Quicktime file?)';
         }
-        
+
         // check last for being valid
         $lastAtom = $this->_fileAtoms[count($this->_fileAtoms) - 1];
-        
+
         if (!Atom::isValidAtom($lastAtom->getType())) {
             $this->_fileValid = false;
             return 'encountered non-QT top-level atom (is this a Quicktime file?';
         }
-        
+
         // store validity
         return $this->_fileValid = true;
     }
@@ -339,7 +342,7 @@ class Moovrelocator
         if ($this->_fileAtoms[count($this->_fileAtoms) - 1]->getType() != Atom::MOOV_ATOM) {
             return 'The moov-atom isn\'t located at the end of the file, the file is allready ready for progressive download or it is a invalid file';
         }
-        
+
         // moov is at end of file...
         return true;
     }
@@ -350,7 +353,7 @@ class Moovrelocator
      *
      * set output file
      *
-     * @param string $filename The filename ( + path) to put result (fixed data) in 
+     * @param string $filename The filename ( + path) to put result (fixed data) in
      *
      * @return  boolean True if everything wents fine, otherwise false
      * @author  Benjamin Carl <phpfluesterer@googlemail.com>
@@ -362,7 +365,7 @@ class Moovrelocator
     {
         // store filename
         $this->_outputFile = $filename;
-        
+
         // success
         return true;
     }
@@ -375,7 +378,7 @@ class Moovrelocator
      *
      * @param string  $filename       The input filename
      * @param string  $outputFilename The output filename
-     * @param boolean $overwrite      The overwrite status 
+     * @param boolean $overwrite      The overwrite status
      *
      * @return  mixed True if everything wents fine, otherwise error-message as string
      * @author  Benjamin Carl <phpfluesterer@googlemail.com>
@@ -390,25 +393,25 @@ class Moovrelocator
         if ($result !== true) {
             return $result;
         }
-        
+
         if (is_null($outputFilename) && $overwrite === true) {
             $fileResult = $filename;
         } else {
             $fileResult = $outputFilename;
         }
-        
+
         // set the output filename and path
         $result = $this->setOutput($fileResult);
         if ($result !== true) {
             return $result;
         }
-        
+
         // moov positioning fix
         $result = $this->fix();
         if ($result !== true) {
             return $result;
         }
-        
+
         // success
         return true;
     }
@@ -418,7 +421,7 @@ class Moovrelocator
      * fix moov-atom location
      *
      * fix moov-atom location
-     * 
+     *
      * @return  mixed True if everything wents fine, otherwise error-message as string
      * @author  Benjamin Carl <phpfluesterer@googlemail.com>
      * @version 0.1
@@ -431,39 +434,39 @@ class Moovrelocator
         if (!$this->_successfullyParsed) {
             return 'please open a file first! (syntax: '.__CLASS__.'->setInput($filename);)';
         }
-        
+
         if (!$this->_outputFile) {
             return 'please set an outputfile first! (syntax: '.__CLASS__.'->setOutput($file);)';
         }
-        
+
         // check if moov atom is allready at beginning of file
         if (!$this->_moovAtomAtEOF()) {
             return 'nothing to do! moov allready at begin of file!';
         }
-        
+
         // Bytearray's holding bytes from file
         $this->_ftypBytes = new Bytearray();
         $this->_middleBytes = new Bytearray();
         $this->_moovBytes = new Bytearray();
-        
+
         // read in file's bytes
         $result = $this->_readBytes();
         if ($result !== true) {
             return $result;
         }
-        
+
         // now start swapping
         $result = $this->_swapIndex();
         if ($result !== true) {
             return $result;
         }
-        
+
         // write new file
         $result = $this->_writeFile();
         if ($result !== true) {
             return $result;
         }
-        
+
         // everythings fine!
         return true;
     }
@@ -473,7 +476,7 @@ class Moovrelocator
      * reads all bytes from all found atoms
      *
      * reads all bytes from all found atoms
-     * 
+     *
      * @return  mixed True if everything wents fine, otherwise error-message as string
      * @author  Benjamin Carl <phpfluesterer@googlemail.com>
      * @version 0.1
@@ -484,28 +487,28 @@ class Moovrelocator
     {
         // read bytes from all found atoms
         for ($atom = 0; $atom < count($this->_fileAtoms); $atom++) {
-            
+
             // get the current atom/box
             $currentAtom = $this->_fileAtoms[$atom];
             $currentAtomType = $currentAtom->getType();
-            
+
             // keep ftyp atom
             if ($currentAtomType == Atom::FTYP_ATOM) {
                 // set file pointer to begin of file
                 fseek($this->_fp, 0);
                 $bytes = fread($this->_fp, $currentAtom->getSize());
                 $this->_ftypBytes->writeBytes($bytes);
-            
+
             } else if ($currentAtomType == Atom::MOOV_ATOM) {
                 $bytes = fread($this->_fp, $currentAtom->getSize());
                 $this->_moovBytes->writeBytes($bytes);
-            
+
             } else {
                 $bytes = fread($this->_fp, $currentAtom->getSize());
                 $this->_middleBytes->writeBytes($bytes);
             }
         }
-        
+
         return true;
     }
 
@@ -514,7 +517,7 @@ class Moovrelocator
      * swaps the index from end to beginning
      *
      * swaps the index from end to beginning
-     * 
+     *
      * @return  mixed True if everything wents fine, otherwise error-message as string
      * @author  Benjamin Carl <phpfluesterer@googlemail.com>
      * @version 0.1
@@ -524,45 +527,45 @@ class Moovrelocator
     private function _swapIndex()
     {
         $moovSize = $this->_moovBytes->bytesAvailable();
-        
+
         $moovAType = '';
         $moovASize = 0;
         $offsetCount = 0;
-        
+
         $compressionCheck = $this->_moovBytes->readBytes(12, 4);
-        
+
         if ($compressionCheck == Atom::CMOV_ATOM) {
             throw new Exception('compressed MP4/QT-file can\'t do this file: '.$file);
         }
-        
+
         // begin of metadata
         $metaDataOffsets = array();
         $metaDataStrings = array();
         $metaDataCurrentLevel = 0;
-        
+
         $moovStartOffset = 12;
-        
+
         for ($i = $moovStartOffset; $i < $moovSize - $moovStartOffset; $i++) {
             $moovAType = $this->_moovBytes->readUTFBytes($i, 4);
-            
+
             if (Atom::isValidAtom($moovAType)) {
-                
+
                 $moovASize = $this->_moovBytes->readUnsignedInt($i - 4);
-                
+
                 if (($moovASize > 8) && ($moovASize + $i < ($moovSize - $moovStartOffset))) {
-                    
+
                     try {
                         $containerLength = 0;
                         $containerString = $moovAType;
-                        
+
                         for ($mi = count($metaDataOffsets) - 1; $mi > - 1; $mi--) {
-                            
+
                             $containerLength = $metaDataOffsets[$mi];
-                            
+
                             if ($i - $moovStartOffset < $containerLength && $i - $moovStartOffset + $moovASize > $containerLength) {
                                 throw new Exception('bad atom nested size');
                             }
-                            
+
                             if ($i - $moovStartOffset == $containerLength) {
                                 array_pop($metaDataOffsets);
                                 array_pop($metaDataStrings);
@@ -570,12 +573,12 @@ class Moovrelocator
                                 $containerString = $metaDataStrings[$mi].".".$containerString;
                             }
                         }
-                        
+
                         if (($i - $moovStartOffset) <= $containerLength) {
                             array_push($metaDataOffsets, ($i - $moovStartOffset + $moovASize));
                             array_push($metaDataStrings, $moovAType);
                         }
-                        
+
                         if ($moovAType != Atom::STCO_ATOM && $moovAType != Atom::CO64_ATOM) {
                             $i += 4;
                         } elseif ($moovAType == Atom::URL_ATOM || $moovAType == Atom::XML_ATOM) {
@@ -588,58 +591,58 @@ class Moovrelocator
                 }
             }
 
-            
+
             if ($moovAType == Atom::STCO_ATOM) {
                 $moovASize = $this->_moovBytes->readUnsignedInt($i - 4);
-                
+
                 if ($i + $moovASize - $moovStartOffset > $moovSize) {
                     throw new Exception('bad atom size');
                     return;
                 }
-                
+
                 $offsetCount = $this->_moovBytes->readUnsignedInt($i + 8);
-                
+
                 for ($j = 0; $j < $offsetCount; $j++) {
                     $position = ($i + 12 + $j * 4);
-                    
+
                     $currentOffset = $this->_moovBytes->readUnsignedInt($position);
-                    
+
                     // cause of mooving the moov-atom right before the rest of data
                     // (behind ftyp) the new offset is caluclated:
                     // current-offset + size of moov atom (box) = new offset
                     $currentOffset += $moovSize;
-                    
+
                     $this->_moovBytes->writeBytes(Transform::toUInt32BE($currentOffset), $position + 1);
                 }
                 $i += $moovASize - 4;
-            
+
             } else if ($moovAType == Atom::CO64_ATOM) {
                 $moovASize = $this->_moovBytes->readUnsignedInt($i - 4);
-                
+
                 if ($i + $moovASize - $moovStartOffset > $moovSize) {
                     throw new Exception('bad atom size');
                     return;
                 }
-                
+
                 $offsetCount = $this->_moovBytes->readDouble($i + 8);
-                
+
                 for ($j2 = 0; $j2 < $offsetCount; $j2++) {
                     $position = ($i + 12 + $j * 8);
-                    
+
                     $currentOffset = $this->_moovBytes->readUnsignedInt($position);
-                    
+
                     // cause of mooving the moov-atom right before the rest of data
                     // (behind ftyp) the new offset is caluclated:
                     // current-offset + size of moov atom (box) = new offset
                     $currentOffset += $moovSize;
-                    
+
                     // TODO implement!
                     //$this->_moovBytes->writeBytes(Transform::toUInt64BE($currentOffset), $position+1);
                 }
                 $i += $moovASize - 4;
             }
         }
-        
+
         return true;
     }
 
@@ -648,7 +651,7 @@ class Moovrelocator
      * write the new byteorder to a new file
      *
      * write the new byteorder to a new file
-     * 
+     *
      * @return  mixed True if everything wents fine, otherwise error-message as string
      * @author  Benjamin Carl <phpfluesterer@googlemail.com>
      * @version 0.1
@@ -659,41 +662,41 @@ class Moovrelocator
     {
         // check if we need to unlink exisiting outputfile
         if (file_exists($this->_outputFile)) {
-            
+
             // close handle
             if ($this->_fp) {
                 @fclose($this->_fp);
                 $this->_fp = null;
             }
-            
+
             if (!@unlink($this->_outputFile)) {
                 return 'error deleting file: '.$this->_outputFile.' outputfile "'.$this->_outputFile.'" exists (overwite = true)!';
             }
         }
-        
+
         // open predefined output file
         if (!$fh = fopen($this->_outputFile, 'wb+')) {
             return 'error opening outputfile: '.$this->_outputFile.' for wb+ access!';
         }
-        
+
         // put ftyp atom/box in
         if (!fwrite($fh, $this->_ftypBytes->readAllBytes())) {
             return 'error writing ftyp-atom to outputfile: '.$this->_outputFile;
         }
-        
+
         // put moov atom in
         if (!fwrite($fh, $this->_moovBytes->readAllBytes())) {
             return 'error writing moov-atom to outputfile: '.$this->_outputFile;
         }
-        
+
         // put rest data in
         if (!fwrite($fh, $this->_middleBytes->readAllBytes())) {
             return 'error writing other atom(s) to outputfile: '.$this->_outputFile;
         }
-        
+
         // close handle
         fclose($fh);
-        
+
         // everything's fine
         return true;
     }
@@ -703,11 +706,11 @@ class Moovrelocator
      * factory for creating atom object instances
      *
      * factory for creating atom object instances
-     * 
+     *
      * @param integer $offset The offset of the atom (in bytes)
      * @param integer $size   The size of the atom (in bytes)
      * @param string  $type   The type of the atom
-     * 
+     *
      * @return  object An instance of the Atom-class
      * @author  Benjamin Carl <phpfluesterer@googlemail.com>
      * @version 0.1
@@ -718,17 +721,40 @@ class Moovrelocator
     {
         // instanciate a new atom-object
         $atom = new Atom();
-        
+
         // setup ...
         $atom->setOffset($offset);
         $atom->setSize($size);
         $atom->setType($type);
-        
+
         // and give back ...
         return $atom;
     }
 
-    
+    /**
+     * resets the state of the last operation to defaults
+     *
+     * This method is intend to reset the state of the last operation to defaults.
+     *
+     * @return  void
+     * @author  Benjamin Carl <phpfluesterer@googlemail.com>
+     * @version 0.1
+     * @since   Method available since Release 0.2
+     * @access  public
+     */
+    private function _reset()
+    {
+        $this->_fileAtoms = array();
+        $this->_lastAtom = null;
+        $this->_successfullyParsed = false;
+        $this->_fileValid = null;
+
+        if ($this->_fp) {
+            @fclose($this->_fp);
+        }
+    }
+
+
     /**
      * instance singleton requester
      *
@@ -748,7 +774,7 @@ class Moovrelocator
         return self::$_instance;
     }
 
-    
+
     /**
      * prevents cloning of the class
      *
@@ -766,7 +792,7 @@ class Moovrelocator
         // empty container
     }
 
-    
+
     /**
      * gets called on garbage collecting (object destroyed)
      *
@@ -783,9 +809,7 @@ class Moovrelocator
     public function __destruct()
     {
         // close handle
-        if ($this->_fp) {
-            @fclose($this->_fp);
-        }
+        $this->_reset();
     }
 }
 
